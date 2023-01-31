ebc229f84b2cd5ccff56d9b8a2398494b0b48504
NouranOmar98 <norankinane@gmail.com>
2023-01-31 20:23:43 +0300

    task

---
+++ 31.01.2023/Image/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Image.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        //public string About()
+        //{
+        //    return "This Image Show A beutiful Animal";
+        //}
+        public string Contact()
+        {
+            return "PLease Tap Twice to Download The Image :))";
+        }
+        public string image()
+        {
+           
+            return ("<a download='Task.jpg' href='../Images/Task.jpg'><img src='../images/Task.jpg'/></a>");
+        }
+        //public FileContentResult DownloadContent()
+        //{
+
+        //    var path = Server.MapPath("../Images/Task.jpg");
+
+        //    Response.AddHeader("Content-Disposition", "attachment;filename=DealerAdTemplate.jpg");
+        //    Response.WriteFile(path);
+        //    Response.End();
+        //    return null;
+        //}
+
+    }
+}
---
+++ 31.01.2023/Image/Controllers/NouranController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Image.Controllers
+{
+    public class NouranController : Controller
+    {
+        // GET: Nouran
+
+        //RedirectToGoogle
+        public ActionResult RedirectToGoogle()
+        {
+            return Redirect("https://www.google.com");
+        }
+
+        //TextData
+        public ContentResult TextData()
+        {
+            string text = "Hello My Name Is Nouran";
+            return Content(text, "text/plain");
+        }
+
+        //NotFound (Error hhh) :)) 
+
+        public ActionResult NotFound()
+        {
+            return View("NotFound", null, HttpStatusCode.NotFound);
+        }
+
+        //
+
+        //public FileResult Download()
+        //{
+        //    byte[] fileBytes = System.IO.File.ReadAllBytes("path/to/file.pdf");
+        //    return File(fileBytes, "application/pdf", "file.pdf");
+        //}
+
+        //
+
+        public JsonResult Contact()
+        {
+            return Json(new { message = "Please Tap Twice to Download The Image :))" });
+        }
+
+    }
+}
