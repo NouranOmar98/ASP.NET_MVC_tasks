2f270b1ee64c6a55bdd4e50f3d02d04d441ab0de
NouranOmar98 <norankinane@gmail.com>
2023-02-06 22:16:55 +0300

    task

---
+++ 31.01.2023/6 FEBRUARY/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using _6_FEBRUARY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _6_FEBRUARY.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        private TaskEntities db = new TaskEntities();
+
+        public ActionResult Customers()
+        {
+            return View(db.Customers.ToList());
+
+        }
+
+        public PartialViewResult Orders()
+        
+        { 
+             
+            return PartialView(db.Orders.ToList().Last());
+        
+        }
+
+    }
+}
